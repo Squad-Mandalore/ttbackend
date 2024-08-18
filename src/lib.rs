30260669e078f154b5db9a62e38d407af935853d
BB223 <79644542+BB223@users.noreply.github.com>
2024-08-18 18:13:24 +0200

    feat: SQUAD-18 graceful shutdown

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,32 @@
+use tokio::signal;
+
 pub mod database;
 pub mod graphql;
-pub mod models;
+mod models;
 mod service;
-pub mod time_utils;
+mod time_utils;
 pub mod tracing_setup;
+
+pub async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+}
