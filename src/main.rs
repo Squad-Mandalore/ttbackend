dc5cd636703e52717e66a1c66cda5ba0d4bc0b95
BB223 <79644542+BB223@users.noreply.github.com>
2024-08-19 21:47:49 +0200

    feat: add swagger-ui stuff

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use axum::{
     routing::{get, post},
     Extension, Router,
 };
+use tower_http::services::ServeFile;
 use ttbackend::{
     auth::{auth, login, refresh},
     database::set_up_database,
@@ -13,6 +14,7 @@ use ttbackend::{
     tracing_setup::{remove_old_logfiles, setup_tracing},
 };
 
+#[cfg(debug_assertions)]
 async fn graphql_playground() -> impl IntoResponse {
     response::Html(playground_source(GraphQLPlaygroundConfig::new("/")))
 }
@@ -28,12 +30,23 @@ async fn main() {
     let schema = create_schema(database_pool.clone());
     // build our application with a single route
     let app = Router::new()
-        .route("/", post(graphql_handler).layer(middleware::from_fn(auth)))
+        .route(
+            "/graphql",
+            post(graphql_handler).layer(middleware::from_fn(auth)),
+        )
         .layer(Extension(schema))
-        .route("/playground", get(graphql_playground))
         .route("/login", post(login))
         .route("/refresh", post(refresh))
         .with_state(database_pool);
+
+    #[cfg(debug_assertions)]
+    let debug = Router::new()
+        .route("/playground", get(graphql_playground))
+        .route_service("/docs", ServeFile::new("docs.html"))
+        .route_service("/api.yaml", ServeFile::new("api.yaml"));
+
+    #[cfg(debug_assertions)]
+    let app = app.nest("/debug", debug);
 
     // run our app with hyper, listening globally on port 3000
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
