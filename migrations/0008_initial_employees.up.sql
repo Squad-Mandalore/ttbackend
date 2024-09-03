2ec843b05b04fa04e466aab222e786b987c83734
Markus Siegert <spongebobmarkus@gmail.com>
2024-09-03 21:28:40 +0200

    ci: SQUAD-51 testing of security.rs and update_password function

--- migrations/0008_initial_employees.up.sql
+++ migrations/0008_initial_employees.up.sql
@@ -1,12 +1,12 @@
 -- Add up migration script here
 INSERT INTO employee (firstname, lastname, password, pw_salt, email, weekly_time, address_id) VALUES
-('Luke', 'Skywalker', 'luke.skywalker@tatooine.com', '', 'luke.skywalker@tatooine.com', '40 hours', 1),
-('Padmé', 'Amidala', 'padme.amidala@naboo.com', '', 'padme.amidala@naboo.com', '35 hours', 2),
-('Han', 'Solo', 'han.solo@hoth.com', '', 'han.solo@hoth.com', '45 hours', 3),
-('Lando', 'Calrissian', 'lando.calrissian@bespin.com', '', 'lando.calrissian@bespin.com', '30 hours', 4),
-('Leia', 'Organa', 'leia.organa@coruscant.com', '', 'leia.organa@coruscant.com', '38 hours', 5),
-('Mace', 'Windu', 'mace.windu@deepcore.com', '', 'mace.windu@deepcore.com', '42 hours', 6),
-('Jar Jar', 'Binks', 'jarjar.binks@naboo.com', '', 'jarjar.binks@naboo.com', '40 hours', 7),
-('Obi-Wan', 'Kenobi', 'obiwan.kenobi@tatooine.com', '', 'obiwan.kenobi@tatooine.com', '45 hours', 8),
-('Darth', 'Vader', 'darth.vader@deathstar.com', '', 'darth.vader@deathstar.com', '48 hours', 9),
-('Yoda', '', 'yoda@dagobah.com', '', 'yoda@dagobah.com', '30 hours', 10);
+('Luke', 'Skywalker', 'luke.skywalker@tatooine.com', null, 'luke.skywalker@tatooine.com', '40 hours', 1),
+('Padmé', 'Amidala', 'padme.amidala@naboo.com', null, 'padme.amidala@naboo.com', '35 hours', 2),
+('Han', 'Solo', 'han.solo@hoth.com', null, 'han.solo@hoth.com', '45 hours', 3),
+('Lando', 'Calrissian', 'lando.calrissian@bespin.com', null, 'lando.calrissian@bespin.com', '30 hours', 4),
+('Leia', 'Organa', 'leia.organa@coruscant.com', null, 'leia.organa@coruscant.com', '38 hours', 5),
+('Mace', 'Windu', 'mace.windu@deepcore.com', null, 'mace.windu@deepcore.com', '42 hours', 6),
+('Jar Jar', 'Binks', 'jarjar.binks@naboo.com', null, 'jarjar.binks@naboo.com', '40 hours', 7),
+('Obi-Wan', 'Kenobi', 'obiwan.kenobi@tatooine.com', null, 'obiwan.kenobi@tatooine.com', '45 hours', 8),
+('Darth', 'Vader', 'darth.vader@deathstar.com', null, 'darth.vader@deathstar.com', '48 hours', 9),
+('Yoda', '', 'yoda@dagobah.com', null, 'yoda@dagobah.com', '30 hours', 10);
--- src/security.rs
+++ src/security.rs
@@ -95,3 +95,173 @@ async fn get_password(pool: &PgPool, employee_id: &i32) -> sqlx::Result<String> 
     .await
     .and_then(|record| Ok(record.password))
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::service::employee::update_password;
+
+    use super::*;
+
+    #[sqlx::test(fixtures(
+        "../fixtures/truncate.sql",
+        "../fixtures/address.sql",
+        "../fixtures/employee.sql",
+    ))]
+    async fn test_get_some_salt(pool: PgPool) -> sqlx::Result<()> {
+        // given is the employee_id
+        let employee_id = 1;
+
+        // when get_salt is called
+        let salt = get_salt(&pool, &employee_id).await?;
+
+        // then salt should be 'no'
+        assert!(salt.is_some());
+        assert_eq!(salt.unwrap(), String::from("no"));
+        Ok(())
+    }
+
+    #[sqlx::test(fixtures(
+        "../fixtures/truncate.sql",
+        "../fixtures/address.sql",
+        "../fixtures/employee.sql",
+    ))]
+    async fn test_get_none_salt(pool: PgPool) -> sqlx::Result<()> {
+        // given is the employee_id
+        let employee_id = 2;
+
+        // when get_salt is called
+        let salt = get_salt(&pool, &employee_id).await?;
+
+        // then the salt should be none
+        assert!(salt.is_none());
+        Ok(())
+    }
+
+    #[sqlx::test(fixtures(
+        "../fixtures/truncate.sql",
+        "../fixtures/address.sql",
+        "../fixtures/employee.sql",
+    ))]
+    async fn test_get_password(pool: PgPool) -> sqlx::Result<()> {
+        // given is the employee_id
+        let employee_id = 2;
+
+        // when get_password is called
+        let password = get_password(&pool, &employee_id).await?;
+
+        // then the password should be catgirls123
+        assert_eq!(password, String::from("catgirls123"));
+        Ok(())
+    }
+
+    #[test]
+    fn test_iterate_hash_length() {
+        // given is a password to get hashed and a KEYCHAIN_NUMBER
+        let password = String::from("catgirls123");
+        const KEYCHAIN_NUMBER: i32 = 187;
+
+        // when iterate_hash is called
+        let hashed_password = iterate_hash(password, KEYCHAIN_NUMBER);
+
+        // then the hashed_password should be a String with 64 hexadecimal characters
+        assert_eq!(64, hashed_password.len());
+    }
+
+    #[test]
+    fn test_iterate_hash_iterations() {
+        // given is a password to get hashed and a KEYCHAIN_NUMBER and another KEYCHAIN_NUMBER
+        let password = String::from("catgirls123");
+        const KEYCHAIN_NUMBER: i32 = 187;
+        const ANOTHER_KEYCHAIN_NUMBER: i32 = 361;
+
+        // when iterate_hash is called with both keychain_numbers
+        let hashed_password = iterate_hash(password.clone(), KEYCHAIN_NUMBER);
+        let another_hashed_password = iterate_hash(password, ANOTHER_KEYCHAIN_NUMBER);
+
+        // then the both hashed_passwords should not match
+        assert_ne!(hashed_password, another_hashed_password);
+    }
+
+    #[sqlx::test(fixtures(
+        "../fixtures/truncate.sql",
+        "../fixtures/address.sql",
+        "../fixtures/employee.sql",
+    ))]
+    async fn test_create_salt(pool: PgPool) -> sqlx::Result<()> {
+        // given is the employee_id and the salt_length
+        let employee_id = 2;
+        const SALT_LENGTH: usize = 31;
+
+        // when create_salt is called
+        create_salt(&pool, &employee_id, SALT_LENGTH).await?;
+
+        // then there should be a salt inside the database with the same length and the SALT_LENGTH
+        let salt = get_salt(&pool, &employee_id).await?;
+        assert!(salt.is_some());
+        assert_eq!(SALT_LENGTH, salt.unwrap().len());
+
+        Ok(())
+    }
+
+    #[sqlx::test(fixtures(
+        "../fixtures/truncate.sql",
+        "../fixtures/address.sql",
+        "../fixtures/employee.sql",
+    ))]
+    async fn test_verify_password_and_hash_password_first_login(pool: PgPool) -> sqlx::Result<()> {
+        // given is the employee_id and the password to verify
+        let employee_id = 2;
+        let given_password = String::from("catgirls123");
+
+        // when verify_password is called
+        let check = verify_password(&pool, &employee_id, given_password).await?;
+
+        // then it should match since it is the first login and no salt was already existing
+        assert!(check);
+
+        Ok(())
+    }
+
+    #[sqlx::test(fixtures(
+        "../fixtures/truncate.sql",
+        "../fixtures/address.sql",
+        "../fixtures/employee.sql",
+    ))]
+    async fn test_update_password(pool: PgPool) -> sqlx::Result<()> {
+        // given is a user which has not logged in yet and its password
+        let employee_id = 1;
+        let password = get_password(&pool, &employee_id).await?;
+
+        // when password is updated
+        update_password(&pool, &employee_id, password).await?;
+
+        // then the password should be hashed and there is a new salt
+        let salt = get_salt(&pool, &employee_id).await?;
+        assert!(salt.is_some());
+        assert_eq!(dotenvy::var("SALT_LENGTH").expect("SALT_LENGTH variable is not set.").parse::<usize>().unwrap(), salt.unwrap().len());
+        assert_eq!(64, get_password(&pool, &employee_id).await?.len());
+
+        Ok(())
+    }
+
+    #[sqlx::test(fixtures(
+        "../fixtures/truncate.sql",
+        "../fixtures/address.sql",
+        "../fixtures/employee.sql",
+    ))]
+    async fn test_verify_password_and_hash_password_on_updated_password(pool: PgPool) -> sqlx::Result<()> {
+        // given is the employee_id and the password to verify on a user which has a proper
+        // password
+        let employee_id = 2;
+        let given_password = String::from("catgirls123!");
+        update_password(&pool, &employee_id, given_password.clone()).await?;
+
+        // when verify_password is called
+        let check = verify_password(&pool, &employee_id, given_password).await?;
+
+        // then it should match since it is the first login and no salt was already existing
+        assert!(check);
+
+        Ok(())
+    }
+}
