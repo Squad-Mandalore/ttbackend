84901d54cd70e2d3a8dea440df2eae9f7a3a7f3d
Markus Siegert <spongebobmarkus@gmail.com>
2024-09-03 11:14:32 +0200

    fix: SQUAD-51 remove initial salt

--- migrations/0008_initial_employees.up.sql
+++ migrations/0008_initial_employees.up.sql
@@ -1,12 +1,12 @@
 -- Add up migration script here
 INSERT INTO employee (firstname, lastname, password, pw_salt, email, weekly_time, address_id) VALUES
-('Luke', 'Skywalker', 'luke.skywalker@tatooine.com', 'saltA1', 'luke.skywalker@tatooine.com', '40 hours', 1),
-('Padmé', 'Amidala', 'padme.amidala@naboo.com', 'saltB2', 'padme.amidala@naboo.com', '35 hours', 2),
-('Han', 'Solo', 'han.solo@hoth.com', 'saltC3', 'han.solo@hoth.com', '45 hours', 3),
-('Lando', 'Calrissian', 'lando.calrissian@bespin.com', 'saltD4', 'lando.calrissian@bespin.com', '30 hours', 4),
-('Leia', 'Organa', 'leia.organa@coruscant.com', 'saltE5', 'leia.organa@coruscant.com', '38 hours', 5),
-('Mace', 'Windu', 'mace.windu@deepcore.com', 'saltF6', 'mace.windu@deepcore.com', '42 hours', 6),
-('Jar Jar', 'Binks', 'jarjar.binks@naboo.com', 'saltG7', 'jarjar.binks@naboo.com', '40 hours', 7),
-('Obi-Wan', 'Kenobi', 'obiwan.kenobi@tatooine.com', 'saltH8', 'obiwan.kenobi@tatooine.com', '45 hours', 8),
-('Darth', 'Vader', 'darth.vader@deathstar.com', 'saltI9', 'darth.vader@deathstar.com', '48 hours', 9),
-('Yoda', '', 'yoda@dagobah.com', 'saltJ0', 'yoda@dagobah.com', '30 hours', 10);
+('Luke', 'Skywalker', 'luke.skywalker@tatooine.com', '', 'luke.skywalker@tatooine.com', '40 hours', 1),
+('Padmé', 'Amidala', 'padme.amidala@naboo.com', '', 'padme.amidala@naboo.com', '35 hours', 2),
+('Han', 'Solo', 'han.solo@hoth.com', '', 'han.solo@hoth.com', '45 hours', 3),
+('Lando', 'Calrissian', 'lando.calrissian@bespin.com', '', 'lando.calrissian@bespin.com', '30 hours', 4),
+('Leia', 'Organa', 'leia.organa@coruscant.com', '', 'leia.organa@coruscant.com', '38 hours', 5),
+('Mace', 'Windu', 'mace.windu@deepcore.com', '', 'mace.windu@deepcore.com', '42 hours', 6),
+('Jar Jar', 'Binks', 'jarjar.binks@naboo.com', '', 'jarjar.binks@naboo.com', '40 hours', 7),
+('Obi-Wan', 'Kenobi', 'obiwan.kenobi@tatooine.com', '', 'obiwan.kenobi@tatooine.com', '45 hours', 8),
+('Darth', 'Vader', 'darth.vader@deathstar.com', '', 'darth.vader@deathstar.com', '48 hours', 9),
+('Yoda', '', 'yoda@dagobah.com', '', 'yoda@dagobah.com', '30 hours', 10);
